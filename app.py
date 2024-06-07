8def17f64ac7a9ef997cd2578f3a3e980f4ed007
Ankita Pawar <pawarankita123@outlook.com>
2024-06-07 20:31:52 +0530

    modifications

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def predict():
     return render_template('result.html', fare_amount=fare_amount)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host = '0.0.0.0', port= 8080)
